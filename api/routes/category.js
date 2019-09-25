74bb1761929bfcdba0a9c00f9811466a08bd2656
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2019-09-25 14:11:52 +0200

    commit

--- api/routes/category.js
+++ api/routes/category.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const checkAuth = require('../middleware/check-auth');
+
 const categoryController = require('../controllers/category');
+
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, '../', '../', 'uploads') + '/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
 
 router.get('/', categoryController.getCategories);
 
-router.post('/', checkAuth, categoryController.addCategory);
+router.post('/', checkAuth, upload.single('image'), categoryController.addCategory);
 
 router.patch('/:categoryId', checkAuth, categoryController.editCategory);
 
